5edf6e24781a5ab09f494dac34bc4cc3272eccfd
YaroslavW <doctor355@yahoo.com>
2018-12-19 14:01:04 +0300

    reate DB - local and test

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/server-side');
 
-const app = express();
+const Cat = mongoose.model('Cat', { name: String });
 
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
 
-const posts = [
-    {
-        title:  "lorem ipsum1",
-        text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elitconsectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-    },
-    {
-        title:  "lorem ipsum2",
-        text: "Lorem ipsum dolor sit amet  Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-    },
-    {
-        title:  "lorem ipsum3",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-    },
-    {
-        title:  "lorem ipsum4",
-        text: "Lorem ipsum dolor sitLorem ipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-    }    
-];
-app.get('/posts', function(req, res) {
-    return res.send(posts);
-});
 
-app.get('/posts/:id', function(req, res) {
-    const id = req.params.id;
-    res.send(posts[id]);
-});
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
-app.post('/posts', function(req, res) {
-    // получаем данные из тела запроса и сохраняем в конст.
-    const data = req.body;
-    // посмотрим что у нас там? 
-    console.log(data);
-    // добавляем полученные данные к постам
-    posts.push(data);
-    // чтобы не было бесконечного цикла - вернем все посты на страницу
-    return res.send(posts);
-});
-app.listen(3000, ()=>console.log('Server running on 3000 port'));
+// const app = express();
+
+// app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(bodyParser.json());
+
+// const posts = [
+//     {
+//         title:  "lorem ipsum1",
+//         text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elitconsectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
+//     },
+//     {
+//         title:  "lorem ipsum2",
+//         text: "Lorem ipsum dolor sit amet  Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
+//     },
+//     {
+//         title:  "lorem ipsum3",
+//         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
+//     },
+//     {
+//         title:  "lorem ipsum4",
+//         text: "Lorem ipsum dolor sitLorem ipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
+//     }    
+// ];
+// app.get('/posts', function(req, res) {
+//     return res.send(posts);
+// });
+
+// app.get('/posts/:id', function(req, res) {
+//     const id = req.params.id;
+//     res.send(posts[id]);
+// });
+
+// app.post('/posts', function(req, res) {
+//     // получаем данные из тела запроса и сохраняем в конст.
+//     const data = req.body;
+//     // посмотрим что у нас там? 
+//     console.log(data);
+//     // добавляем полученные данные к постам
+//     posts.push(data);
+//     // чтобы не было бесконечного цикла - вернем все посты на страницу
+//     return res.send(posts);
+// });
+// app.listen(3000, ()=>console.log('Server running on 3000 port'));
