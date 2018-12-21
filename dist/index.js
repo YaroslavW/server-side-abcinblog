b4328e9c654665702c839fd180b570f94fb25532
YaroslavW <doctor355@yahoo.com>
2018-12-21 11:50:06 +0300

    Creating a controller

--- dist/index.js
+++ dist/index.js
@@ -12,12 +12,15 @@ var _bodyParser = require('body-parser');
 
 var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
-var _Post = require('./models/Post');
+var _PostController = require('./controllers/PostController');
 
-var _Post2 = _interopRequireDefault(_Post);
+var _PostController2 = _interopRequireDefault(_PostController);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var Post = new _PostController2.default();
+// import PostModel from './models/Post'; перенесли в контроллер.
+// import Post from './models/Post'; перенесли в контроллер.
 var app = (0, _express2.default)();
 
 _mongoose2.default.connect('mongodb://localhost/server-side');
@@ -25,49 +28,12 @@ _mongoose2.default.connect('mongodb://localhost/server-side');
 app.use(_bodyParser2.default.urlencoded({ extended: true }));
 app.use(_bodyParser2.default.json());
 
-app.post('/posts', function (req, res) {
-    var data = req.body;
-    // console.log(req.body);
-    var post = new _Post2.default({
-        title: data.title,
-        text: data.text
-    });
-
-    post.save().then(function () {
-        res.send({ status: "OK" });
-    });
-});
-
-app.get('/posts', function (req, res) {
-    _Post2.default.find().then(function (err, posts) {
-        if (err) {
-            res.send(err);
-        }
-        res.json(posts);
-    });
-});
-
-app.delete('/posts/:id', function (req, res) {
-    _Post2.default.remove({
-        _id: req.params.id
-    }).then(function (post) {
-        if (post) {
-            res.json({ status: "deleted" });
-        } else {
-            res.json({ status: "error" });
-        }
-    });
-});
-
-app.put('/posts/:id', function (req, res) {
-    _Post2.default.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err) {
-        if (err) {
-            res.send(err);
-        }
-        res.json({ status: "update" });
-    });
-});
+app.get('/posts', Post.index);
+app.get('/posts/:id', Post.read);
+app.post('/posts', Post.create);
+app.delete('/posts/:id', Post.delete);
+app.put('/posts/:id', Post.update);
 
 app.listen(3000, function () {
-    return console.log("Server running on 3000 port");
+  return console.log("Server running on 3000 port");
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import bodyParser from 'body-parser';
-import PostModel from './models/Post';
-import Post from './models/Post';
+import PostController from './controllers/PostController';
+const Post = new PostController();
+// import PostModel from './models/Post'; перенесли в контроллер.
+// import Post from './models/Post'; перенесли в контроллер.
 const app = express();
 
 mongoose.connect('mongodb://localhost/server-side');
@@ -10,47 +12,10 @@ mongoose.connect('mongodb://localhost/server-side');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/posts', (req, res) => {
-    const data = req.body;
-    // console.log(req.body);
-    const post = new PostModel({
-        title: data.title,
-        text: data.text
-    });
-
-    post.save().then(() => {
-        res.send({ status: "OK"});
-    });
-});
-
-app.get('/posts', (req, res) => {
-    Post.find().then((err, posts) => {
-        if(err){
-            res.send(err);
-        }
-        res.json(posts);
-    });
-});
-
-app.delete('/posts/:id', (req, res) => {
-    PostModel.remove({
-        _id: req.params.id
-    }).then(post => {
-        if(post) {
-            res.json({ status: "deleted"});
-        } else {
-            res.json({ status: "error"});
-        }
-    });
-});
-
-app.put('/posts/:id', (req, res) => {
-    PostModel.findByIdAndUpdate(req.params.id, {$set: req.body}, (err) => {
-        if(err) {
-            res.send(err);
-        }
-        res.json({ status: "update"});
-    });
-});
+app.get('/posts', Post.index);
+app.get('/posts/:id', Post.read);
+app.post('/posts', Post.create);
+app.delete('/posts/:id', Post.delete);
+app.put('/posts/:id', Post.update);
 
 app.listen(3000, () => console.log("Server running on 3000 port"));
